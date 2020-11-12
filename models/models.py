cf2f848943e9af8af210d0e0c329fc288aa30955
Your Name <you@example.com>
2020-11-12 13:47:09 -0300

    Almacenar stock min y max en stock.quant

--- models/models.py
+++ models/models.py
@@ -6,8 +6,8 @@ class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     stock_min = fields.Float(
-        string='Stock Mínimo',related="product_tmpl_id.reordering_min_qty",
+        string='Stock Mínimo',related="product_tmpl_id.reordering_min_qty",store=True,
         required=False)
     stock_max = fields.Float(
-        string='Stock Máximo',related="product_tmpl_id.reordering_max_qty",
+        string='Stock Máximo',related="product_tmpl_id.reordering_max_qty",store=True,
         required=False)
